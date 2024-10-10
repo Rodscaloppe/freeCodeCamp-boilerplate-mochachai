5f9e72d7a3d74e39f532c26c83c0bbc2b5c39a14
Rodolfo Scaloppe <roscaloppe@gmail.com>
2024-10-10 03:56:46 +0000

    teste 1,1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,12 +1,15 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
+suite("Unit Tests", function () {
+  suite("Basic Assertions", function () {
     // #1
-    test('#1 isNull, isNotNull', function () {
-      assert.isNull(null, ' null is null');
-      assert.isNotNull(1, '1 is not null');
+    test("#isNull, #isNotNull", function () {
+      assert.isNull(
+        null,
+        "This is an optional error description - e.g. null is null"
+      );
+      assert.isNotNull(1, "1 is not null");
     });
     // #2
     test('#isDefined, #isUndefined', function () {
